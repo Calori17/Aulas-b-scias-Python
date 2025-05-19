90123f197ea259bf02fe80800519450fa43ad1ef
Calori <arthurcalori1@gmail.com>
2025-05-19 17:08:17 -0300

    Atividade aula 06

---
+++ projetos.py/aulas/aula06.py
@@ -0,0 +1,10 @@
+numero_1 = int(input("Digite o número 1:"))
+
+numero_2 = int(input("Digite o número 2:"))
+
+if numero_1 > numero_2:
+    print ("O número 1 é maior que o número 2.")
+elif numero_2 > numero_1:
+    print ("O número 2 é maior que o número 1.")
+elif numero_1 == numero_2:
+    print ("O número 1 é igual que o número 2")
