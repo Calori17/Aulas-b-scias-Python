a6f7aede7e7f6b6ea175ffb3c85cc2bcba9b9146
Calori <arthurcalori1@gmail.com>
2025-05-19 23:31:24 -0300

    Corrigindo código com o uso de and

--- projetos.py/aulas/aula03.py
+++ projetos.py/aulas/aula03.py
@@ -7,11 +7,15 @@ if idade >= 18:
 else:
     print (f'Olá, {nome}. Não podemos permitir sua entrada! Você possui apenas {idade} anos.')
 
-sobremesa = str(input('Você deseja uma sobremesa gratuita?'))
+sobremesa = str(input('Você deseja uma sobremesa gratuita? '))
 
-if sobremesa == 'Sim' or 'Sim.':
-    print ("Tudo bem, aguarde um momento que logo iremos trazer a sobremesa.")
-elif sobremesa == 'Não':
-    print("Poxa, tudo bem. Mas ela é otima, esperamos que na proxima você aceite.")
+certeza = str(input("Tem certeza? Ela acabou de ser feita. "))
+
+resposta = 'Tenho certeza'
+
+if (sobremesa == 'Sim' or sobremesa == 'sim') and certeza != resposta:
+    print("Tudo bem, aguarde um momento que logo iremos trazer a sobremesa.")
+elif sobremesa == 'Não' or sobremesa == 'não':
+    print("Poxa, tudo bem. Mas ela é ótima, esperamos que na próxima você aceite.")
 else:
-    print("Desculpa, não entendi ")
+    print("Desculpa, não entendi.")
