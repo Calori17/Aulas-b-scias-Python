94d52d714eafcc11fa310b088fbed074a119d4e5
Calori <arthurcalori1@gmail.com>
2025-05-26 18:23:40 -0300

    Adicionando básicods Try e Except

---
+++ projetos.py/aulas/aula09.py
@@ -0,0 +1,14 @@
+# try
+# except
+
+numero_div = float(input("Digite um número para ser divido: "))
+divisao = float(input("Por qual número deseja dividir? "))
+
+
+resultado = numero_div / divisao
+
+try: 
+    print(f"O resultado é {resultado}")
+
+except:
+    print("Nenhum número foi digitado.")
