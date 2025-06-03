083ecf2e744b0f83bc6f0c2ab3cb9290d8322156
Calori <arthurcalori1@gmail.com>
2025-06-02 23:55:51 -0300

    Tabuada usando while

---
+++ projetos.py/aulas/aula14.py
@@ -0,0 +1,10 @@
+contador = 0
+
+while contador >= 0:
+    contador = contador + 1
+    print (f'Numero: {contador}')
+
+
+    if contador == 100:
+        print ("Numero maximo antigido.")
+        break
