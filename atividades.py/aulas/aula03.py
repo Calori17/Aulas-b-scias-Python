bada4f919d05f97aa9b015d602dcab0c2bfacf33
Arthur Calori <arthurcalori1@gmail.com>
2025-06-20 19:50:29 -0300

    Corringindo erro

--- atividades.py/aulas/aula03.py
+++ atividades.py/aulas/aula03.py
@@ -5,7 +5,7 @@ idade = int(input('Digite sua idade:'))
 
 
 if idade >= 18:
-    print (f'Seja muito bem vindo {nome}! Você possui {idade} anos.Pode entrar, você é maior de idade!')    
+    print (f'Seja muito bem vindo {nome}! Você possui {idade} anos. Pode entrar, você é maior de idade!')    
 else:
     print (f'Olá, {nome}. Não podemos permitir sua entrada! Você possui apenas {idade} anos.')
 
